9b35d56d2544a9ca7effde3797b8aacde01d9bf5
Ashutosh-Soni111 <80837903+Ashutosh-Soni111@users.noreply.github.com>
2023-05-03 16:59:08 +0530

    First Commit

---
+++ Assets/Data/Data.tsx
@@ -0,0 +1,176 @@
+
+// import Amethyste from '../Images/Améthyste 6mm 182px.jpeg';
+// import Apatite from "../Images/Apatite 4mm 108px.jpeg";
+// import Aventurine from "../Images/Aventurine Vert 8mm 223px.jpeg";
+// import BlueLaceAgate from "../Images/Blue lace agate 4mm 108px.jpeg";
+// import Calcedoine from "../Images/Calcedoine 6mm Full Size.jpeg";
+// import Cornaline from "../Images/Cornaline 4mm 108px.jpeg";
+// import Grenat from "../Images/Grenat 8mm 322px.jpeg";
+// import Dumortierite from "../Images/Dumortiérite 8mm 223px.jpeg";
+
+
+
+export const Beades = [
+    {
+        value: require("../Images/Améthyste 6mm 182px.jpeg"),
+        label: "Amethyste 6mm",
+    },
+    {
+        value: require("../Images/Apatite 4mm 108px.jpeg"),
+        label: "Apatite 4mm",
+    },
+    {
+        value: require("../Images/Aventurine Vert 8mm 223px.jpeg"),
+        label: "Aventurine 8mm",
+    },
+    {
+        value: require("../Images/Blue lace agate 4mm 108px.jpeg"),
+        label: "BlueLaceAgate 4mm",
+    },
+    {
+        value: require("../Images/Calcedoine 6mm Full Size.jpeg"),
+        label: "Calcedoine 6mm",
+    },
+    {
+        value: require("../Images/Cornaline 4mm 108px.jpeg"),
+        label: "Cornaline 4mm",
+    },
+    {
+        value: require("../Images/Dumortiérite 8mm 223px.jpeg"),
+        label: "Dumortierite 8mm",
+    },
+    {
+        value: require("../Images/Grenat 8mm 322px.jpeg"),
+        label: "Grenat 8mm",
+    },
+];
+export const Golds = [
+    {
+        value: require('../Images/Améthyste 6mm 182px.jpeg'),
+        label: "Amethyste 6mm",
+    },
+    {
+        value: require("../Images/Apatite 4mm 108px.jpeg"),
+        label: "Apatite 4mm",
+    },
+];
+export const Silvers = [
+    {
+        value: require("../Images/Aventurine Vert 8mm 223px.jpeg"),
+        label: "Aventurine 8mm",
+    },
+    {
+        value: require("../Images/Blue lace agate 4mm 108px.jpeg"),
+        label: "BlueLaceAgate 4mm",
+    },
+];
+export const StanlessSteel = [
+    {
+        value: require("../Images/Calcedoine 6mm Full Size.jpeg"),
+        label: "Calcedoine 6mm",
+    },
+    {
+        value: require("../Images/Cornaline 4mm 108px.jpeg"),
+        label: "Cornaline 4mm",
+    },
+    {
+        value: require("../Images/Grenat 8mm 322px.jpeg"),
+        label: "Grenat 8mm",
+    },
+];
+export const ButtonName = [
+    { name: "Beades", value: Beades },
+    { name: "Gold", value: Golds },
+    { name: "Silver", value: Silvers },
+    { name: "Stanles_Steel", value: StanlessSteel },
+];
+export const BeadSize = [
+    {
+        value: "4",
+        label: "4mm",
+    },
+    {
+        value: "8",
+        label: "8mm",
+    },
+    {
+        value: "12",
+        label: "12mm",
+    },
+    {
+        value: "24",
+        label: "24mm",
+    },
+];
+export const wristSize = [
+    {
+        value: "6",
+        label: "6mm",
+    },
+    {
+        value: "8",
+        label: "8mm",
+    },
+    {
+        value: "10",
+        label: "10mm",
+    },
+    {
+        value: "12",
+        label: "12mm",
+    },
+];
+export const Deg = [
+    [
+        "0",
+        "90",
+        "180",
+        "270",
+        "45",
+        "135",
+        "225",
+        "315",
+        "30",
+        "150",
+        "210",
+        "330",
+        "60",
+        "120",
+        "240",
+        "300",
+        "15",
+        "165",
+        "195",
+        "345",
+        "75",
+        "105",
+        "255",
+        "285",
+    ],
+    [
+        "0",
+        "90",
+        "180",
+        "270",
+        "30",
+        "150",
+        "210",
+        "330",
+        "60",
+        "120",
+        "240",
+        "300",
+        "45",
+        "135",
+        "225",
+        "315",
+        "15",
+        "165",
+        "195",
+        "345",
+        "75",
+        "105",
+        "255",
+        "285",
+    ],
+];
